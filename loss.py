fa7dfd56d9c09a5f642855f89ccac5ea2f9849c4
timeywl <1125386393@qq.com>
2020-07-01 10:35:11 +0800

    delete files

--- loss.py
+++
@@ -1,9 +0,0 @@
-from torch.nn import functional as F 
-
-def cos_loss(m1, m2):
-    
-    return F.cosine_similarity(m1, m2, dim=1)
-
-def euc_loss(m1, m2):
-    
-    return F.pairwise_distance(m1, m2, p=2)
